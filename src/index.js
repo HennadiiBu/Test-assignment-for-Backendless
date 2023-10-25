e208ff2e532cfd0a9e61e578bd5e3cd67c9001ce
Hennadii Bublikov <99074724+HennadiiBu@users.noreply.github.com>
2023-10-25 11:43:07 +0300

    2

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter  basename="/goit-react-hw-05-movies/tabs">
+    <BrowserRouter  basename="/Test-assignment-for-Backendless/tabs">
       <App />
     </BrowserRouter>
   </React.StrictMode>
