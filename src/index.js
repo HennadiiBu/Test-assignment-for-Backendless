da7577eab61295541805674207f9a55bfe370282
Hennadii Bublikov <99074724+HennadiiBu@users.noreply.github.com>
2023-10-26 17:29:26 +0300

    5

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter  basename="/Test-assignment-for-Backendless">
+    <BrowserRouter  basename="/Test-assignment-for-Backendless/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
